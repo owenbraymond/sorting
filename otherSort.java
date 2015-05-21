21b4d8e8b38c1840b18d2fe7ec6338d344409221
Owen <owenbraymond@gmail.com>
2015-05-21 11:24:44 -0500

    final commit

---
+++ otherSort.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class otherSort implements sortingAlgorithm 
+{
+    public void sort(int[] array)
+    {
+          for(int i = 0; i < array.length; i ++)
+        {
+            for(int k = i + 1; k < array.length; k ++)
+            {
+                if(array[i] > array[k])
+                {
+                    int temp = array[i];
+                    array[i] = array[k];
+                    array[k] = temp;
+                }
+            }
+        }
+    }
+}
